3a2bd59b7cc027754d1ef5bd87f755f4783760f0
maazmemon <mmemon4151@gmail.com>
2021-10-16 22:49:58 +0530

    init

--- Home.js
+++ Home.js
@@ -1,7 +1,7 @@
-// console.log("hello");
-// alert('warning you are learning JS')
-// var b ='hello maaz ';
-// console.log(b)
+console.log("hello");
+alert('warning you are learning JS')
+var b ='hello maaz ';
+console.log(b)
 var JsAge = prompt("what is your age");
 
 document.getElementById("fromindex").innerHTML=JsAge
