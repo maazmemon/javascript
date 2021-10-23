844d8721d82541ec935e293026853bdfa20c803d
maazmemon <mmemon4151@gmail.com>
2021-10-23 20:31:10 +0530

    init

--- Home.js
+++ Home.js
@@ -1,7 +1,24 @@
-console.log("hello");
-alert('warning you are learning JS')
-var b ='hello maaz ';
-console.log(b)
-var JsAge = prompt("what is your age");
+// console.log("hello");
+// alert('warning you are learning JS')
+// var b ='hello maaz ';
+// console.log(b)
+// var JsAge = promt("what is your age");
 
-document.getElementById("fromindex").innerHTML=JsAge
+// document.getElementById("fromindex").innerHTML=JsAge
+prompt("what is your name");
+
+var Score = Math.random() * 100;
+Score = Math.floor(Score)+1;
+
+
+if(Score > 70){
+    alert("your amazing.....!! " + Score + "%");
+}
+if(Score >30 && Score<=70){
+    alert("your Good....!! " + Score + "%");
+}
+if (Score <=30){
+    alert("your worst.....!! " + Score + "%");
+    
+
+}
